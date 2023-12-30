dbf1a6d37e765c0f3a949aba2458efa68420ecf4
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2023-12-30 10:36:03 -0400

    solve 1006 - média 2

---
+++ 1006.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<fixed<<setprecision(1);
+
+    double a=0,b=0,c=0,media=0;
+
+    cin>>a>>b>>c;
+
+    media = (a*2+b*3+c*5)/10;
+
+    cout<<"MEDIA = "<<media<<endl;
+
+    return 0;
+}
