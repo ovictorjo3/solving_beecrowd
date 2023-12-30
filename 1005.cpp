d08855642c9d84a0a9913905aa9318f03817ae0a
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2023-12-30 10:12:34 -0400

    solve  1005 - Média 1

---
+++ 1005.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<fixed<<setprecision(5);
+
+    double a=0, b=0, media=0;
+
+    cin>>a>>b;
+
+    media = (a*3.5+b*7.5)/11;
+
+    cout<<"MEDIA = "<<media<<endl;
+    return 0;
+}
