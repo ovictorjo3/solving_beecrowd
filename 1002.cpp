f42ad5212a053dd31766b1b142009653bc96eb45
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2023-12-30 09:41:29 -0400

    solve  1002 - Área do Círculo

---
+++ 1002.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    cout<<fixed<<setprecision(4);
+
+    double raio=0, area=0, opi=3.14159;
+
+    cin>>raio;
+
+    area = opi * (raio * raio);
+
+    cout<<"A="<<area<<endl;
+
+    return 0;
+}
