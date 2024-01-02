93f1c5b5a3a75ebd75d20583188c626a60e02778
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2024-01-02 10:05:33 -0400

    solved 1014 - consumo

---
+++ 1014.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<fixed<<setprecision(3);
+
+    double x,y,consumo;
+
+    cin>>x>>y;
+
+    consumo = (x/y);
+
+    cout<<consumo<<" km/l"<<endl;
+    
+    return 0;
+}
